75a18f57424731f6f119deae7411ee46484298ba
MOSBAHI Abdelaziz <mosbehiasiz@gmail.com>
2025-10-03 10:26:35 +0100

    first commit

---
+++ src/app/bestseller/bestseller.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bestseller',
+  standalone: false,
+  templateUrl: './bestseller.component.html',
+  styleUrl: './bestseller.component.css'
+})
+export class BestsellerComponent {
+
+}
